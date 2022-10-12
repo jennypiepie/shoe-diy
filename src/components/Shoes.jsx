f0c28ed5bdf66212be4a07fab75a1d3a6e002038
派派 <2416655925@qq.com>
2022-10-13 01:25:47 +0800

    Toggles the color and pattern selections

--- src/components/Shoes.jsx
+++ src/components/Shoes.jsx
@@ -10,7 +10,7 @@ function Shoes(props) {
   const { nodes, materials } = useGLTF('/shoe-draco.glb')
   const [hovered, set] = useState(null)
   const { gl, scene, camera } = useThree()
-  const [colorMap] =  useTexture(['/textures/texture8.jpg'])
+  const colorMap =  useTexture(Store.pattern[snap.current])
 
   if (snap.screenshot===true) {
     gl.render(scene, camera)
@@ -19,7 +19,7 @@ function Shoes(props) {
   }
 
   useEffect(() => {
-    const cursor = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><g filter="url(#filter0_d)"><path d="M29.5 47C39.165 47 47 39.165 47 29.5S39.165 12 29.5 12 12 19.835 12 29.5 19.835 47 29.5 47z" fill="${snap.items[hovered]}"/></g><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/><text fill="#000" style="white-space:pre" font-family="Inter var, sans-serif" font-size="10" letter-spacing="-.01em"><tspan x="35" y="63">${hovered}</tspan></text></g><defs><clipPath id="clip0"><path fill="#fff" d="M0 0h64v64H0z"/></clipPath><filter id="filter0_d" x="6" y="8" width="47" height="47" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="2"/><feGaussianBlur stdDeviation="3"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/><feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/><feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/></filter></defs></svg>`
+    const cursor = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><g filter="url(#filter0_d)"><path d="M29.5 47C39.165 47 47 39.165 47 29.5S39.165 12 29.5 12 12 19.835 12 29.5 19.835 47 29.5 47z" fill="${snap.color[hovered]}"/></g><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/><text fill="#000" style="white-space:pre" font-family="Inter var, sans-serif" font-size="10" letter-spacing="-.01em"><tspan x="35" y="63">${hovered}</tspan></text></g><defs><clipPath id="clip0"><path fill="#fff" d="M0 0h64v64H0z"/></clipPath><filter id="filter0_d" x="6" y="8" width="47" height="47" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="2"/><feGaussianBlur stdDeviation="3"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/><feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/><feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/></filter></defs></svg>`
     const auto = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/></svg>`
     if (hovered) {
       document.body.style.cursor = `url('data:image/svg+xml;base64,${btoa(cursor)}'), auto`
@@ -36,22 +36,21 @@ function Shoes(props) {
   })
 
 
-  
   return (
     <group ref={ref} {...props} dispose={null}
       onPointerOver={(e) => { e.stopPropagation(); set(e.object.material.name)}}
       onPointerOut={(e) => { e.intersections.length === 0 && set(null) }}
       onPointerDown={(e) => { e.stopPropagation(); Store.current = e.object.material.name}}
-      onPointerMissed={(e) => { Store.current = null }}
+      // onPointerMissed={(e) => { Store.current = null }}
     >
-      <mesh geometry={nodes.shoe.geometry} material={materials.laces} material-color={snap.items.laces} />
-      <mesh geometry={nodes.shoe_1.geometry} material={materials.mesh} material-color={snap.items.mesh} material-map={colorMap} />
-      <mesh geometry={nodes.shoe_2.geometry} material={materials.caps} material-color={snap.items.caps}/>
-      <mesh geometry={nodes.shoe_3.geometry} material={materials.inner} material-color={snap.items.inner}/>
-      <mesh geometry={nodes.shoe_4.geometry} material={materials.sole} material-color={snap.items.sole}/>
-      <mesh geometry={nodes.shoe_5.geometry} material={materials.stripes} material-color={snap.items.stripes}/>
-      <mesh geometry={nodes.shoe_6.geometry} material={materials.band} material-color={snap.items.band}/>
-      <mesh geometry={nodes.shoe_7.geometry} material={materials.patch} material-color={snap.items.patch}/>
+      <mesh geometry={nodes.shoe.geometry} material={materials.laces} material-color={snap.color.laces} />
+      <mesh geometry={nodes.shoe_1.geometry} material={materials.mesh} material-color={snap.color.mesh} material-map={colorMap} />
+      <mesh geometry={nodes.shoe_2.geometry} material={materials.caps} material-color={snap.color.caps}/>
+      <mesh geometry={nodes.shoe_3.geometry} material={materials.inner} material-color={snap.color.inner}/>
+      <mesh geometry={nodes.shoe_4.geometry} material={materials.sole} material-color={snap.color.sole}/>
+      <mesh geometry={nodes.shoe_5.geometry} material={materials.stripes} material-color={snap.color.stripes}/>
+      <mesh geometry={nodes.shoe_6.geometry} material={materials.band} material-color={snap.color.band}/>
+      <mesh geometry={nodes.shoe_7.geometry} material={materials.patch} material-color={snap.color.patch}/>
     </group>
   )
 }
--- src/stores/Store.js
+++ src/stores/Store.js
@@ -1,8 +1,12 @@
 import { proxy } from "valtio";
+import texture1 from '../assets/textures/texture1.jpg'
+import texture2 from '../assets/textures/texture2.jpg'
+import texture3 from '../assets/textures/texture3.jpg'
+import texture4 from '../assets/textures/texture4.jpg'
 
 const Store = proxy({
-  current: null,
-  items: {
+  current: 'mesh',
+  color: {
     laces: '#ffffff',
     mesh: '#ffffff',
     caps: '#ffffff',
@@ -11,6 +15,48 @@ const Store = proxy({
     stripes: '#ffffff',
     band: '#ffffff',
     patch: '#ffffff'
+  },
+  pattern: {
+    laces:null,
+    mesh: texture1,
+    caps: null,
+    inner: null,
+    sole: null,
+    stripes: null,
+    band:null,
+    patch:null
+  },
+  laces: {
+    color: ['#ffffff','#000000','#70cccc']
+  },
+  mesh:{
+    color:["#db858f", "#30432d", "#e9b229", "#5b4691", "#c7e3e9","#063a78", "#d6d3c6", "#74433f","#063a78", "#d6d3c6", "#74433f"],
+    pattern:[texture1,texture2,texture3,texture4],
+    material: []
+  },
+  caps: {
+    color: ["#db858f", "#30432d", "#e9b229", "#5b4691"],
+    pattern:[texture1,texture2,texture3,texture4],
+  },
+  inner: {
+    color: ["#c7e3e9", "#063a78"],
+    pattern:[texture1,texture2,texture3,texture4],
+  },
+  sole: {
+    color: ["#db858f"],
+    pattern:[texture1,texture2,texture3,texture4],
+  },
+  stripes: {
+    color: ["#d6d3c6", "#74433f"],
+    pattern:[texture1,texture2,texture3,texture4],
+  },
+  band: {
+    color: ["#d6d3c6", "#74433f"],
+    pattern:[texture1,texture2,texture3,texture4],
+  },
+  patch: {
+    color: ["#d6d3c6", "#74433f"],
+    pattern:[texture1,texture2,texture3,texture4],
   },
   screenshot:false,
   imgUrls:[]
